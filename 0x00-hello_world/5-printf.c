fc2e8ba73ea53515465ee67a465c18dac190f269
Adeola <oniadeola33@gmail.com>
2023-04-22 07:02:53 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *  Return: 0 always sucess
 */
 
-int main(void)
+int main( void )
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return(0);
+        printf( "with proper grammar, but the outcome is a piece of art,\n" );
+        return( 0 );
 }
