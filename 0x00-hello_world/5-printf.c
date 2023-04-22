a73392153d6e6160bcff0b2968886a3c717824b7
Adeola <oniadeola33@gmail.com>
2023-04-22 07:05:55 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - entry point
- * prints with proper grammar, but the outcome is a piece of art, followed by a new line
+ * prints with proper grammar, but the outcome is a piece of art, 
+ * followed by a new line
  *  Return: 0 always sucess
 */
 
-int main( void )
+int main(void)
 {
-        printf( "with proper grammar, but the outcome is a piece of art,\n" );
-        return( 0 );
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
